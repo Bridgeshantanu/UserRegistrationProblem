b014c5d1f221e214c5723edf33365ae6d2c01347
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-09 14:23:27 +0530

    Add: mobile format code is added

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ namespace Test
 
             //string namepattern = "^[A-Z]{1}[a-z]{2,}$";
             //string namepattern = "^[a-z]{3,}[ ][A-Z]{1}[a-z]{2,}$";
-            string namepattern = "^[0-9a-zA-Z]+[.][0-9a-zA_z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
+            //string namepattern = "^[0-9a-zA-Z]+[.][0-9a-zA_z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
+            string namepattern = "^[0-9]{2}[ ][0-9]{10}$";
             Regex regex = new Regex(namepattern);
             if (regex.IsMatch(name))
             {
