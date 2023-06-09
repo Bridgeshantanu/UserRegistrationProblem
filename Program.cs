2891b1e5b72cb91a812d7e878adae62cfc012b18
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-09 13:58:03 +0530

    Add: valid first name code is added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+
+            string namepattern = "^[A-Z]{1}[a-z]{2,}$";
+            Regex regex = new Regex(namepattern);
+            if (regex.IsMatch(name))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("invald");
+                Console.ResetColor();
+            }
+        }
+    }
+}
